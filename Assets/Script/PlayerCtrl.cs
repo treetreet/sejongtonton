0f266fd87fc0c9c83424f4f9cda31ef49eac18e5
treetreet <treetreet@kakao.com>
2025-05-09 20:06:13 +0900

    Update playerCtrl
    add move

--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -1,12 +1,17 @@
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerCtrl : MonoBehaviour
 {
     InputSystem_Actions inputsystem;
     InputAction moveAction; // (-1, 0) a (1, 0) d (0, 1) w (0, -1) s
     InputAction lookAction; //-1 q, 1 e
+    [SerializeField] Tilemap blockedTilemap;
 
     void Start()
     {
@@ -18,6 +23,17 @@ public class PlayerCtrl : MonoBehaviour
 
     void Update()
     {
+        Vector3 targetWorldPos = transform.position + (Vector3)moveAction.ReadValue<Vector2>();
+        Vector3Int targetCellPos = blockedTilemap.WorldToCell(targetWorldPos);
+        if (blockedTilemap.HasTile(targetCellPos))
+        {
+            Debug.Log("이동 불가! 장애물 있음");
+        }
+        else 
+        {
+            transform.position += new Vector3(targetCellPos.x, targetCellPos.y, transform.position.z);
+        }
+
         Debug.Log(moveAction.ReadValue<Vector2>());
         Debug.Log(lookAction.ReadValueAsObject());
     }
